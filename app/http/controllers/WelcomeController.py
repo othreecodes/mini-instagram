1edc088b1352dfb802925af20cba1244f66cd833
Obi Uchenna David <daviduchenna@outlook.com>
2018-08-17 23:35:34 +0100

    initial commit

---
+++ app/http/controllers/WelcomeController.py
@@ -0,0 +1,9 @@
+''' Welcome The User To Masonite '''
+
+class WelcomeController:
+    ''' Controller For Welcoming The User '''
+
+    def show(self, Application):
+        ''' Show Welcome Template '''
+        self.
+        return view('welcome', {'app': Application})
